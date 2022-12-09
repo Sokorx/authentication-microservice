c1f32a90e603580208109af66410bab37a264b09
ikechukwu kalu <ikechukwukalu@ikechukwus-MacBook-Pro.local>
2022-12-09 18:14:11 +0100

    Added database cron backup

--- app/Console/Commands/DatabaseBackUp.php
+++ app/Console/Commands/DatabaseBackUp.php
@@ -31,7 +31,7 @@ class DatabaseBackUp extends Command
         $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".gz";
 
         $command = "ssh -t " . env('DB_USERNAME') . "@" . env('DB_HOST') . " '" . env('DATABASE_BACKUP_COMMAND') . env('DATABASE_BACKUP') . "/" . $filename . " && exit; exec bash -l'";
-dd($command);
+
         $returnVar = NULL;
         $output  = NULL;
 
